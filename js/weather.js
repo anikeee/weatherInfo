1b6df09a3595669f3e1a1ad4ef59d773a605feca
MD Muid Hasan Anik <muid@tripiq.eu>
2023-02-01 15:20:01 +0000

    wip : putting weather data in index file

--- js/weather.js
+++ js/weather.js
@@ -3,15 +3,29 @@ const API_KEY = "51d0772ba951b254bef717f872899c0b";
 
 // function to get weather information for a specific city
 function getWeather(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=51d0772ba951b254bef717f872899c0b`)
-        .then(response => response.json())
-        .then(data => {
-            // console.log the data for weather information for the entered city
-            console.log(data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    const apiKey = '51d0772ba951b254bef717f872899c0b';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // Get the current date and format it using Moment.js
+    const currentDate = moment().format('MMM Do YYYY');
+
+    // Get the temperature, humidity, and wind speed from the data
+    const temperature = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+
+    // Update the content of the <section id="today"> element
+    const today = document.querySelector('#today');
+    today.innerHTML = `
+    <h2 class="text-center">Current Weather for ${data.name}</h2>
+    <p class="text-center">${currentDate}</p>
+    <p class="text-center">Temperature: ${temperature}°F</p>
+    <p class="text-center">Humidity: ${humidity}%</p>
+    <p class="text-center">Wind Speed: ${windSpeed} mph</p>
+  `;
 }
 
 // Get the form and add a submit event listener
