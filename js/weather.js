bc24f1bd6a84541104e36cd7e127dc184d528192
MD Muid Hasan Anik <muid@tripiq.eu>
2023-02-01 15:13:13 +0000

    getting data from api

--- js/weather.js
+++ js/weather.js
@@ -2,13 +2,30 @@
 const API_KEY = "51d0772ba951b254bef717f872899c0b";
 
 // function to get weather information for a specific city
-async function getWeather(city) {
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    const data = await response.json();
-    return data;
+function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=51d0772ba951b254bef717f872899c0b`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log the data for weather information for the entered city
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
+
+// Get the form and add a submit event listener
+const form = document.querySelector("#search-form");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    // Get the value of the city entered in the form
+    const city = document.querySelector("#search-input").value;
+
+    // Call the getWeather function with the city as a parameter
+    getWeather(city);
+});
+
 
 // function to get forecast information for a specific city
 async function getForecast(city) {
